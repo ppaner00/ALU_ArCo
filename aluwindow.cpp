2efa056b532e3b42a476afc0ced20a8e3dd109ee
Davidmoref <dmoref00@estudiantes.unileon.es>
2022-03-31 23:19:27 +0200

    Estructura ui
    This reverts commit 9cb73f7eb6853a0020fee5a140c08c90d0ce8c43.

--- aluwindow.cpp
+++ aluwindow.cpp
@@ -1,6 +1,7 @@
 #include "aluwindow.h"
 #include "ui_aluwindow.h"
 
+
 ALUWindow::ALUWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::ALUWindow)
@@ -11,5 +12,129 @@ ALUWindow::ALUWindow(QWidget *parent)
 ALUWindow::~ALUWindow()
 {
     delete ui;
+}
+
+
+void ALUWindow::on_Sumar_clicked()
+{
+    double sol = sumar();
+    ui->SolReal->setText(QString::number(sol));
+}
+
+
+void ALUWindow::on_Multiplicar_clicked()
+{
+
+}
+
+
+void ALUWindow::on_Dividir_clicked()
+{
+
+}
+
+
+double ALUWindow::sumar(){
+    double sumando1, sumando2, sol;
+    sumando1 = ui->NumeroReal_1->toPlainText().toDouble();
+    sumando2 = ui->NumeroReal_2->toPlainText().toDouble();
+    sol = sumando1 + sumando2;
+    return sol;
+}
+
+double ALUWindow::multiplicar(){
+    double sumando1, sumando2, sol;
+    sumando1 = ui->NumeroReal_1->toPlainText().toDouble();
+    sumando2 = ui->NumeroReal_2->toPlainText().toDouble();
+    sol = sumando1 * sumando2;
+    return sol;
+}
+
+double ALUWindow::dividir(){
+    double sumando1, sumando2, sol;
+    sumando1 = ui->NumeroReal_1->toPlainText().toDouble();
+    sumando2 = ui->NumeroReal_2->toPlainText().toDouble();
+    sol = sumando1 / sumando2;
+    return sol;
+}
+
+void ALUWindow::on_pushButton_clicked()
+{
+    QMessageBox mensaje;
+    QString texto;
+    texto.append("Manejar el ALU: \n");
+    texto.append("Rellena los cuadros de texto de los operandos de los que quieras calcular la solución. \n");
+    texto.append("Pulsa el botón de la operación que quieras realizar. \n");
+    texto.append("La solución aparecera en un texto en la parte inferior de la ventana.");
+    mensaje.setText(texto);
+    mensaje.exec();
+}
+
+//  ---------------------------
+//  TRANSFORMACIONES DE NUMEROS
+//  ---------------------------
+
+//  INotifyPropertyChanged
+
+//Cuando introduzcas un numero en Real que se rellenen automaticamente los de IEE y Hex
+
+//REAL
+void ALUWindow::on_NumeroReal_1_textChanged()
+{
+    double realToIEEE = ui->NumeroReal_1->toPlainText().toDouble();
+    double realToHex =realToIEEE;
+
+    realToIEEE = realToIEEE * 2; //Aqui hacemos la transformacion llamando a otro metodo
+    realToHex = realToHex * 3;
+
+    ui->NumeroIEEE_1->setText(QString::number(realToIEEE));
+    ui->NumeroHex_1->setText(QString::number(realToHex));
+}
+
+
+void ALUWindow::on_NumeroReal_2_textChanged()
+{
+    double realToIEEE = ui->NumeroReal_2->toPlainText().toDouble();
+    double realToHex =realToIEEE;
+
+    realToIEEE = realToIEEE * 2; //Aqui hacemos la transformacion llamando a otro metodo
+    realToHex = realToHex * 3;
+
+    ui->NumeroIEEE_2->setText(QString::number(realToIEEE));
+    ui->NumeroHex_2->setText(QString::number(realToHex));
+}
+
+
+
+
+//IEEE
+void ALUWindow::on_NumeroIEEE_1_textChanged()
+{
+//    double IEEEToReal = ui->NumeroIEEE_1->toPlainText().toDouble();
+//    double IEEEToHex =IEEEToReal;
+
+//    IEEEToReal = IEEEToReal * 2; //Aqui hacemos la transformacion llamando a otro metodo
+//    IEEEToHex = IEEEToHex * 3;
+
+//    ui->NumeroReal_1->setText(QString::number(IEEEToReal));
+//    ui->NumeroHex_1->setText(QString::number(IEEEToHex));
+}
+
+
+void ALUWindow::on_NumeroIEEE_2_textChanged()
+{
+
+}
+
+
+void ALUWindow::on_NumeroHex_1_textChanged()
+{
+
+}
+
+
+void ALUWindow::on_NumeroHex_2_textChanged()
+{
+
 }
 
--- aluwindow.h
+++ aluwindow.h
@@ -2,6 +2,7 @@
 #define ALUWINDOW_H
 
 #include <QMainWindow>
+#include "QMessageBox"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class ALUWindow; }
@@ -14,6 +15,33 @@ class ALUWindow : public QMainWindow
 public:
     ALUWindow(QWidget *parent = nullptr);
     ~ALUWindow();
+
+    double sumar();
+    double multiplicar();
+    double dividir();
+
+private slots:
+    void on_Sumar_clicked();
+
+    void on_Multiplicar_clicked();
+
+    void on_Dividir_clicked();
+
+    void on_pushButton_clicked();
+
+    void on_NumeroReal_1_textChanged();
+
+    void on_NumeroReal_2_textChanged();
+
+    void on_NumeroIEEE_1_textChanged();
+
+    void on_NumeroIEEE_2_textChanged();
+
+    void on_NumeroHex_1_textChanged();
+
+    void on_NumeroHex_2_textChanged();
+
+    void on_info_clicked();
 
 private:
     Ui::ALUWindow *ui;
