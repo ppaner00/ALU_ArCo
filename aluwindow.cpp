1d4e8c2c1949ccea8e336e72db10de924cf81bb1
Davidmoref <dmoref00@estudiantes.unileon.es>
2022-05-02 18:46:25 +0200

    Real a IEEE (sin terminar)

--- aluwindow.cpp
+++ aluwindow.cpp
@@ -80,30 +80,102 @@ void ALUWindow::on_info_clicked()
 //REAL
 void ALUWindow::on_NumeroReal_1_textChanged()
 {
-    double realToIEEE = ui->NumeroReal_1->toPlainText().toDouble();
-    double realToHex =realToIEEE;
+    //long realToIEEE754 = ui->NumeroReal_1->toPlainText().toLong();
+    //double realToHex =realToIEEE754;
 
-    realToIEEE = realToIEEE * 2; //Aqui hacemos la transformacion llamando a otro metodo
-    realToHex = realToHex * 3;
+    realToIEEE754(); //Aqui hacemos la transformacion llamando a otro metodo
 
-    ui->NumeroIEEE_1->setText(QString::number(realToIEEE));
-    ui->NumeroHex_1->setText(QString::number(realToHex));
+    // realToHex = realToHex * 3;
+
+    ui->NumeroIEEE_1->setText(realToIEEE754());
+    //ui->NumeroHex_1->setText(QString::number(realToHex));
 }
 
 
 void ALUWindow::on_NumeroReal_2_textChanged()
 {
-    double realToIEEE = ui->NumeroReal_2->toPlainText().toDouble();
-    double realToHex =realToIEEE;
+//    double realToIEEE = ui->NumeroReal_2->toPlainText().toDouble();
+//    double realToHex =realToIEEE;
 
-    realToIEEE = realToIEEE * 2; //Aqui hacemos la transformacion llamando a otro metodo
-    realToHex = realToHex * 3;
+    realToIEEE754();         //Aqui hacemos la transformacion llamando a otro metodo
+//    realToHex = realToHex * 3;
 
-    ui->NumeroIEEE_2->setText(QString::number(realToIEEE));
-    ui->NumeroHex_2->setText(QString::number(realToHex));
+    ui->NumeroIEEE_2->setText(realToIEEE754());
+//    ui->NumeroHex_2->setText(QString::number(realToHex));
 }
 
 //  ---------------------------
 //          ALGORITMOS
 //  ---------------------------
 
+QString ALUWindow::realToIEEE754(){
+    long num = ui->NumeroReal_1->toPlainText().toLong();
+    int parteFracc = 23;
+    int contador = 0;
+    int expo = 127;
+
+    QString signo("");
+    QString mantisa("");
+    QString numBinario("");
+    QString auxExpo("");
+    QString P_fraccionaria("");
+    QString numIEEE754("");
+
+    if(num < 0){
+        signo = "1";
+    }else{
+        signo = "0";
+    }
+
+    //pasamos el numero a binario
+    while(true){
+        numBinario = num % 2 ? "1" + numBinario : "0" + numBinario;
+        num /= 2;
+
+        if(num == 0){
+            break;
+        }
+    }
+
+
+    //rellenamos con ceros hasta tener 24 bits
+    for(int i = numBinario.size() - 1;  i < parteFracc; i++){
+        numBinario = numBinario + "0";
+        contador++;
+    }
+
+
+    //sacamos el exponente
+    for(int j = numBinario.size() - (contador + 1); j > 0; j--){
+        expo++;
+    }
+
+
+    //Convertimos el exponente en binario
+    while(true){
+        auxExpo = (expo % 2) ? "1" + auxExpo: "0" + auxExpo;
+        expo /= 2;
+
+        if(expo == 0){
+            break;
+        }
+    }
+
+
+    //sacamos la parte fraccionaria
+    for(int k = 1; k < 24; k++){
+        P_fraccionaria = P_fraccionaria + numBinario[k];
+
+    }
+
+
+//    ui->size->setText(QString::number(numBinario.size()));
+//    ui->mantisa->setText(QString(numBinario));
+//    ui->mantisaSize->setText(QString::number((numBinario.size())));
+//    ui->exponente->setText(QString(auxExpo));
+//    ui->parteFracc->setText((P_fraccionaria));
+//    ui->signo->setText(signo);
+    numIEEE754 = signo + auxExpo + P_fraccionaria;
+//    ui->NumeroIEEE_1->setText(numIEEE754);
+    return numIEEE754;
+}
